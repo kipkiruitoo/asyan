5ce9155c0d2617677af2ffbf39602be9477e4a06
unknown <lincoln.kantet@gmail.com>
2020-02-28 17:06:54 +0300

    additional

--- inventory/models.py
+++ inventory/models.py
@@ -31,22 +31,52 @@ class Pallet(models.Model):
         
         
 class Products(models.Model):
+    
     UNIT_CHOICES = (
         ('Kilograms', 'Kilograms'),
         ('Liters', 'Liters'),
         ('Meters', 'Meters'),
+        ('Piece', 'Piece'),
+        ('Tonne', 'Tonne'),
+        
+
+    )
+    VED_TYPE = (
+        ('Vital', 'Vital'),
+        ('Essential', 'Essential'),
+        ('Desirable', 'Desirable'),
+        
+        
+
+    )
+    INVENTORY_TYPE = (
+        ('Vital', 'Vital'),
+        ('Essential', 'Essential'),
+        ('Desirable', 'Desirable'),  
+
+    )
+    MB_TYPE= (
+        ('make', 'Make'),
+        ('buy', 'Buy'),
+        
 
     )
     name = models.CharField(max_length=255, unique=True)
-    selling_price = models.PositiveIntegerField()	
-    cost_price = models.PositiveIntegerField()	
-    quantity = models.PositiveIntegerField()
-    quantity_available = models.PositiveIntegerField(null=True)
-    unit_measure = models.CharField(max_length=20, null=False, choices=UNIT_CHOICES)
-
-    reoder_level = models.PositiveIntegerField()	
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
     pallet = models.ForeignKey(Pallet, related_name='products', on_delete=models.CASCADE)
+    
+    VED_analysis = unit_measure = models.CharField(max_length=20, null=False,default='', choices=VED_TYPE)
+    buy_or_make=unit_measure = models.CharField(max_length=20, null=False,default='', choices=MB_TYPE)
+    selling_price = models.PositiveIntegerField(null=True)	
+    cost_price = models.PositiveIntegerField(null=True)	
+    quantity_available = models.PositiveIntegerField(null=True)
+    unit_measure = models.CharField(max_length=20, null=False, choices=UNIT_CHOICES)
+    ABC_category= models.CharField(max_length=2,null=True)
+    FSN_analysis= models.CharField(max_length=255,null=True)
+    reoder_level = models.PositiveIntegerField(default='')
+    safety_stock_level = models.PositiveIntegerField()
+    item_description = models.CharField(max_length=255, default='')	
+    
 
     def __str__(self):
         return self.name
