b07c535976f97e2b0a7b5cd7b0489841f60cd031
unknown <lincoln.kantet@gmail.com>
2020-03-05 09:33:16 +0300

    Phone

--- accounts/models.py
+++ accounts/models.py
@@ -8,18 +8,18 @@ from django.contrib.auth.models import Group
 class User(AbstractUser):
     username = models.CharField(max_length=30)
     email = models.EmailField(unique=True)
-
+    head=models.BooleanField(default=False)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
     groups = models.ForeignKey(Group, null=True, on_delete=models.CASCADE)
-
+    phone= models.IntegerField(max_length=15, default=0)
     def __str__(self):
         return self.username
     
 class profile(models.Model):
     user =models.OneToOneField(User, on_delete=models.CASCADE)
     image= models.ImageField(default='default.jpg', upload_to='profile_pics')
-    phone= models.CharField(max_length=15)
+    
     
     def __str__(self):
         return f'{self.user} Profile'
